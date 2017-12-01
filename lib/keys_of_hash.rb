b3e846dba6ed0113a42cc892dcedc91a76f94a5e
Anna Kallenborn-Bolden <a.kallenbornbolden@gmail.com>
2017-12-01 16:46:56 +0000

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,17 @@
+require 'pry'
+
 class Hash
-  def keys_of(arguments)
-    # code goes here
+  def keys_of(*arguments)
+    output= []
+    self.each do |key,value|
+      counter=0
+      while counter < arguments.size
+        if value == arguments[counter]
+          output << key
+        end
+        counter+=1
+      end
+    end
+    output
   end
 end
